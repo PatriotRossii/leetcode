66749e9f2a78b0c292048d5f97f7a3b7d4bd0d2a
PatriotRossii <patriotrossii2019@mail.ru>
2022-02-09 19:18:58 +0500

    Add optimal solution

--- medium/furthest_building_you_can_build/main.cpp
+++ medium/furthest_building_you_can_build/main.cpp
@@ -1,25 +1,42 @@
+#include <queue>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        size_t resultIdx = 0;
-        for(size_t j = 1; j != heights.size(); ++j) {
-            if(heights[resultIdx] >= heights[j]) {
-                resultIdx = j;
-            } else {
-                if(bricks >= heights[j] - heights[j - 1]) {
-                    bricks -= heights[j] - heights[j - 1];
-                    resultIdx = j;
-                } else if(ladders > 0) {
-                    ladders -= 1, resultIdx += 1;
-                } else {
-                    break;
-                }
+        priority_queue<int, std::vector<int>, std::greater<int>> diffs;
+        for(int i = 0; i != heights.size() - 1; ++i) {
+            int diff = heights[i + 1] - heights[i];
+            clog << "Diff: " << diff << '\n';
+
+            if(diff > 0) {
+                diffs.push(diff);
+                clog << diff << " > 0, pushing into priority queue" << '\n';
             }
-        } 
-        return resultIdx;
+            if(diffs.size() > ladders) {
+                clog << diffs.size() << " > ladders, get top from priority queue" << '\n';
+    
+                clog << "Count of bricks before: " << bricks << ", ";
+                bricks -= diffs.top();
+                clog << "count of bricks after: " << bricks << '\n';
+
+                diffs.pop();
+            }
+            if(bricks < 0) {
+                clog << bricks << " < 0, returning " << i << "\n";
+                return i;
+            }
+            clog << '\n';
+        }
+        return heights.size() - 1;
     }
 };
+
+int main() {
+    Solution solution;
+    std::vector<int> heights = {1, 5, 1, 2, 3, 4, 10000};
+    clog << solution.furthestBuilding(heights, 4, 1);
+}
