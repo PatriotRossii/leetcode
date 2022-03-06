af6e77c994835a870065a07ec52cb241a3dd5ed5
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-06 15:57:53 +0500

    Add try on 'Binary search'

---
+++ easy/binary_search/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)
+        while end - start:
+            mid = start + (end - start) // 2
+            if nums[mid] > target:
+                end = mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                return mid
+        return -1
