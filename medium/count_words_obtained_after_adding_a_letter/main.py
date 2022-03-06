153820b9bf632842566980002113cab6d633329b
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-06 16:03:56 +0500

    [medium] Add try on 'Count words obtained after adding a letter'

---
+++ medium/count_words_obtained_after_adding_a_letter/main.py
@@ -0,0 +1,11 @@
+def f(x, y):
+    if x == y:
+        return 1
+    if x > y or x == 16:
+        return 0
+    else:
+        return f(x + 1, y) + f(x * 2, y)
+
+print(
+    f(1, 10) * f(10, 21)
+)
