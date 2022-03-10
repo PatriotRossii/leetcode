2e864e0a0a47ef765b8774bfe2e073ee42d0ef9b
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-10 17:28:55 +0500

    [easy] Add solution of 'Design parking system'

---
+++ easy/design_parking_system/main.py
@@ -0,0 +1,14 @@
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        self.spaces = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        carType -= 1
+        if self.spaces[carType] == 0:
+            return False
+        self.spaces[carType] -= 1
+        return True
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
