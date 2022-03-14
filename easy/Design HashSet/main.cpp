30f9eb7bab04a6c80c02f10364de7db3398e8579
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-14 17:35:21 +0500

    [easy] Solve 'Design HashSet' problem

---
+++ easy/Design HashSet/main.cpp
@@ -0,0 +1,26 @@
+class MyHashSet {
+    static constexpr int MAX_KEY = 1000000;
+    bool data[MAX_KEY + 1] { false };
+public:
+    MyHashSet() { }
+    
+    void add(int key) {
+        data[key] = true;
+    }
+    
+    void remove(int key) {
+        data[key] = false;
+    }
+    
+    bool contains(int key) {
+        return data[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
